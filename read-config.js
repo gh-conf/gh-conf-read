fea6587bcc4e2a4bbbd0be1aaa692ae532a0d3d4
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-02-01 23:04:34 +0530

    adds gh-constants, version upgrade to 1.1.0

--- read-config.js
+++ read-config.js
@@ -3,13 +3,11 @@ const fs = require('fs');
 const { formatError } = require('@gh-conf/gh-conf-response')
 const { validatePath } = require('@gh-conf/gh-conf-validate');
 const { addSlash } = require('@gh-conf/gh-conf-path');
-
-// Constants
-const GIT_CONFIG_PATH = '.git/config'
+const { GH_PATH } = require('@gh-conf/gh-conf-constants');
 
 
 /**
- * Reads git config, by appending GIT_CONFIG_PATH to input path
+ * Reads git config, by appending GITHUB_CONFIG_PATH to input path
  * @param {String} path 
  */
 const readConf = (path) => {
@@ -17,16 +15,16 @@ const readConf = (path) => {
   return new Promise((resolve, reject) => {
 
     // Validate input
-    validatePath(path, GIT_CONFIG_PATH)
+    validatePath(path, GH_PATH['CONFIG'])
 
     // Check and add ending slash if does not exists
     path = addSlash(path);
 
     // Read config by path
-    fs.readFile(`${path}${GIT_CONFIG_PATH}`, 'utf8', function (err, content) {
+    fs.readFile(`${path}${GH_PATH['CONFIG']}`, 'utf8', function (err, content) {
       if (err) {
 
-        reject(formatError(`${path}${GIT_CONFIG_PATH} not found`, err));
+        reject(formatError(`${path}${GH_PATH['CONFIG']} not found`, err));
       }
 
       resolve(content);
